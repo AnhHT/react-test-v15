b2fac2e84450ed12b7d3ff683c666c36dcf562d4
Anh Hoang <anh.hta88@gmail.com>
2016-04-11 15:03:29 +0700

    AnhHT: update something

--- src/views/GetData/DataView.js
+++ src/views/GetData/DataView.js
@@ -3,6 +3,7 @@ import CardContainer from './CardContainer'
 import {actions as manageData} from '../../redux/modules/Todos'
 import {connect} from 'react-redux'
 import classes from './DataView.scss'
+import Select from 'react-select'
 
 const mapStateToProps = (state) => ({
   isUploading: state.todo.isUploading,
@@ -35,12 +36,11 @@ export default class DataView extends Component {
 
   constructor (props) {
     super(props)
-    this.handleSelectedField = this.handleSelectedField.bind(this)
-    this.handleRemoveField = this.handleRemoveField.bind(this)
+    this.handleSelectChange = this.handleSelectChange.bind(this)
     this.state = {
       fileData: null,
-      selectedFields: [],
-      fieldList: []
+      fieldList: [],
+      value: []
     }
   }
 
@@ -50,41 +50,24 @@ export default class DataView extends Component {
 
   componentWillReceiveProps (nextProps) {
     if (nextProps.isFetchFields) {
-      this.setState({fieldList: nextProps.mappingFields})
+      let temp = []
+      temp = nextProps.mappingFields.map((item) => {
+        return {label: item.Name, value: `${item.Id}`, ...item}
+      })
+
+      this.setState({fieldList: temp})
     }
   }
 
-  handleSelectedField (e) {
-    let id = e.target.getAttribute('data-id')
-    let value = e.target.getAttribute('data-value')
-    let index = parseInt(e.target.getAttribute('data-index'))
-    this.setState({
-      selectedFields: [...this.state.selectedFields, {Id: id, Name: value}]
-    }, () => {
-      let temp = this.state.fieldList
-      this.setState({
-        fieldList: [...temp.slice(0, index), ...temp.slice(index + 1)]
-      })
-    })
-  }
-
-  handleRemoveField (e) {
-    let id = e.target.getAttribute('data-id')
-    let value = e.target.getAttribute('data-value')
-    let index = parseInt(e.target.getAttribute('data-index'))
-    this.setState({
-      fieldList: [...this.state.fieldList, {Id: id, Name: value}]
-    }, () => {
-      let temp = this.state.selectedFields
-      this.setState({
-        selectedFields: [...temp.slice(0, index), ...temp.slice(index + 1)]
-      })
-    })
+  handleSelectChange (value) {
+    this.setState({ value: value })
   }
 
   handleSubmit (e) {
     e.preventDefault()
-    this.props.uploadFile(this.state.fileData)
+    if (this.state.fileData) {
+      this.props.uploadFile(this.state.fileData)
+    }
   }
 
   handleFile (e) {
@@ -98,32 +81,17 @@ export default class DataView extends Component {
 
   render () {
     let key = 1
-    const mappingFields = this.state.fieldList.map((item, idx) =>
-      <span key={item.Id} className={classes['field-badge'] + ' badge'} data-index={idx}
-        data-id={item.Id} data-value={item.Name} onClick={this.handleSelectedField}>{item.Name}
-      </span>)
-    const selectedFields = this.state.selectedFields.map((item, idx) =>
-      <span key={item.Id} className={classes['field-badge'] + ' badge'} data-index={idx}
-        data-id={item.Id} data-value={item.Name} onClick={this.handleRemoveField}>{item.Name}
-      </span>)
-
     const data = this.props.isUploaded ? (<CardContainer key={key} previewFile={this.props.data}
       gHeader={this.props.getMappingHeader} headers={this.props.mappingHeader} parseXlsx={this.props.parseXlsx}
       isFetch={this.props.isFetchHeader}/>) : (this.props.isUploading ? <div>loading...</div> : <div></div>)
     return (
       <div className={classes.tempView}>
-        <div style={{paddingBottom: 10, paddingTop: 10}}>
-          <div>
-            <div>
-              <h3>Danh sách mapping</h3>
-              <div>{mappingFields}</div>
+        <div style={{paddingBottom: 10, paddingTop: 10, width: '50%'}}>
+          <form encType='multipart/form-data'>
+            <div className='form-group'>
+              <Select multi simpleValue value={this.state.value} placeholder='Select field'
+                options={this.state.fieldList} onChange={this.handleSelectChange} />
             </div>
-            <div>
-              <h3>Đã chọn</h3>
-              <div>{selectedFields}</div>
-            </div>
-          </div>
-          <form className='form-inline' encType='multipart/form-data'>
             <div className='form-group' style={{paddingRight: 10}}>
               <input type='file' onChange={::this.handleFile} className='form-control'/>
             </div>
